6cea6d16a40a78cc35aa9310c227ad3f20f909a7
cyril jardin <cyilroc60@hotmail.fr>
2018-07-26 15:35:20 +0200

    condition

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>exo5</title>
+  </head>
+  <body>
+    <?php
+$gender = 'femme';
+if ($gender != 'homme') {
+  echo 'C\'est une développeuse !!!';
+}
+elseif ($gender == 'homme') {
+echo 'C\'est un développeur !!!';
+}
+ ?>
+  </body>
+</html>
