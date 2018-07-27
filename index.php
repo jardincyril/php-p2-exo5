8c30cd98ffb2abc6e9b3b88d04e3270be99f114d
cyril jardin <cyilroc60@hotmail.fr>
2018-07-27 16:10:04 +0200

    php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8" />
     <title>exo5</title>
   </head>
   <body>
@@ -9,9 +9,8 @@
 $gender = 'femme';
 if ($gender != 'homme') {
   echo 'C\'est une développeuse !!!';
-}
-elseif ($gender == 'homme') {
-echo 'C\'est un développeur !!!';
+} else {
+  echo 'C\'est un développeur !!!';
 }
  ?>
   </body>
